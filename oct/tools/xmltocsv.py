92706dadada63d4ec0a606c1d554237641ed8fef
Thibs <bn.thibault@gmail.com>
2014-12-17 12:26:52 +0100

    Add parsing XML to CSV

---
+++ oct/tools/xmltocsv.py
@@ -0,0 +1,22 @@
+from __future__ import absolute_import
+import argparse
+from xml.dom.minidom import parse
+import csv
+
+def sitemap_to_csv():
+    parser = argparse.ArgumentParser(description="Convert XML file to CSV file")
+    parser.add_argument("xml", metavar='xml', type=str, nargs=1, help="The XML file to parse")
+    parser.add_argument("csv", metavar='csv', type=str, nargs=1, help="The CSV file to generate")
+    args = parser.parse_args()
+
+    try:
+        datasource = open(args.xml[0], "r")
+        dom = parse(datasource)
+    except IOError:
+        raise IOError("Bad XML file provided")
+
+    with open(args.csv[0], 'wb') as opencsv:
+        urls = dom.getElementsByTagName('url')
+        for url in urls:
+            writer = csv.writer(opencsv, delimiter=';', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            writer.writerow([url.getElementsByTagName('loc')[0].firstChild.nodeValue])
