f1c22325205d0fe6522f6251503ee5fc9e38be86
karec <manu.valette@gmail.com>
2015-01-02 02:38:59 +0100

    update config

--- doc/conf.py
+++ doc/conf.py
@@ -17,7 +17,8 @@ import os
 
 import mock
 
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate', 'mechanize', 'requests']
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate', 'mechanize', 'requests',
+                'cssselect']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
