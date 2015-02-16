cd1488f85cf933f76e0b72b031cec5db31413677
karec <manu.valette@gmail.com>
2015-02-16 22:22:18 +0100

    start refactoring turret file for zmq

--- oct/core/turret.py
+++ oct/core/turret.py
@@ -1,21 +1,10 @@
 from __future__ import print_function
-from celery import Celery, group, current_task
-from celery.utils.log import get_task_logger
 from oct.multimechanize.core import load_script
-from multiprocessing.sharedctypes import Array
 import threading
 import time
 import sys
-import logging
 import os
-
-# This is the main app, update it inside your project if you need to
-app = Celery('oct', backend=None, broker=None)
-
-logger = get_task_logger(__name__)
-buff = Array("i", range(2))
-buff[0] = 0
-buff[1] = 0
+import zmq
 
 
 def main_loop(project_name, cmd_opts, config, output_dir):
@@ -29,52 +18,139 @@ def main_loop(project_name, cmd_opts, config, output_dir):
     script_prefix = os.path.join(cmd_opts.projects_dir, project_name, "test_scripts")
     script_prefix = os.path.normpath(script_prefix)
 
-    user_groups = []
-    threads = 0
+    turrets = []
     for i, ug_config in enumerate(config.user_group_configs):
+        # now we sending messages here, no more instance of anything except the report manager
         script_file = os.path.join(script_prefix, ug_config.script_file)
-        ug = UserGroup(process_num=i, group_name=ug_config.name, thread_num=ug_config.num_threads,
-                       script_file=script_file, output_dir=output_dir,
-                       run_time=config.run_time, rampup=config.rampup, console_logging=config.console_logging)
-        user_groups.append(ug)
-        threads += ug_config.num_threads
+        turret = Turret(cannons=ug_config.num_threads,
+                        script_file=script_file, output_dir=output_dir,
+                        run_time=config.run_time, rampup=config.rampup, turret_config=ug_config)
+        turrets.append(turret)
 
-    res = group(run_ug.s(ug) for ug in user_groups)()
-
-    if config.console_logging:
-        res.get(timeout=config.run_time + 5)
-    else:
-        print('\nuser_groups: {0}'.format(len(user_groups)), end='')
-        print('\tthreads: {0}'.format(threads))
-
-        st = 0
-        info_str = 'time : {0}/{1} seconds transactions: {2} errors: {3}'
-        while not res.ready():
-            try:
-                info = res.results[0].info
-                if not info and not res.ready:
-                    print(info_str.format(st, config.run_time, 0, 0), end='\r')
-                elif info is not None:
-                    print(info_str.format(st, config.run_time, info['trans'], info['errors']), end='\r')
-                time.sleep(1)
-                st += 1
-            except KeyboardInterrupt:
-                # purge all tasks in case of user interruption
-                print('\nPurging all tasks...')
-                app.control.purge()
-                # revoke all tasks in case of KeyBoardInterrupt
-                res.revoke(terminate=True)
-                sys.exit(1)
-            except EnvironmentError:
-                if st <= 1:
-                    # pass if we just start the test and the worker need a warmup
-                    pass
+    for turret in turrets:
+        turret.fire()
 
 
-class Agent(threading.Thread):
+class ReportManager(object):
     """
-    An object representing a agent for executing the script. Inherit from thread class and will be start within celery
-     task
+    This class will receive all messages from turrets and display them for user
+
+    :param listen_url: the url for listening for the results
+    :type listen_url: str
+    :param output_dir: the directory for writing the results
+    :type output_dir: str
+    """
+    def __init__(self, listen_url, output_dir):
+        context = zmq.Context()
+
+        self.socket = context.socket(zmq.PULL)
+        self.socket.bind(listen_url)
+        self.output_dir = output_dir
+        self.data = {
+            'transactions': 0,
+            'errors': 0
+        }
+
+    def receive_message(self):
+
+        message = {}
+
+        # listen while turret send stop message
+        # the message will be sent when turret have done firing
+        # TODO wait untill all turrets have done firing
+        while 'stop' not in message.keys() and not message['stop']:
+            mess = self.socket.recv_json()
+            if 'trans' in mess.keys():
+                self.data['transactions'] += mess['trans']
+            if 'errors' in mess.keys():
+                self.data['errors'] += mess['errors']
+
+            print("transactions : {trans}   errors : {err}".format(trans=self.data['transactions'],
+                                                                   err=self.data['errors']), end='\r')
+
+            with open(self.output_dir + 'results.csv', 'w') as f:
+                f.write(mess['report'])
+                f.flush()
+
+
+class Turret(object):
+    """
+    Represent a Turret object that will use Cannon objects to execute scripts
+
+    :param canons: the number of canons (threads) for this Turret
+    :type cnanons: int
+    :param script_file: the script file to execute
+    :type script_file: str
+    :param output_dir: the directory for the results
+    :type output_dir: str
+    :param run_time: the total time of run
+    :type run_time: int
+    :param rampup: the rampup param, representing the incrementation of cannons over time in seconds
+    :type rampup: int
+    :param turret_config: a dict containing the configuration variables for the turret
+    :type turret_config: dict
+    :param report_manager: the report manager instance for sending the results
+    :type report_manager: ReportManager
+    """
+    def __init__(self, cannons, script_file, output_dir, run_time, rampup, turret_config, report_manager):
+        self.cannons = cannons
+        self.script_file = script_file
+        self.script_module = load_script(self.script_file)
+        self.run_time = run_time
+        self.rampup = rampup
+        self.start_time = time.time()
+        self.output_dir = output_dir
+        self.report_manager = report_manager
+
+        # set the turret configuration and setup the turret
+        context = zmq.Context()
+
+        self.receiver = context.socket(zmq.SUB)
+        self.receiver.connect(turret_config['connection_string'])
+        self.receiver.setsockopt(zmq.SUBSCRIBE, turret_config['topic'])
+
+        # set the report socket configuration
+        self.sender = context.socket(zmq.PUSH)
+        self.sender.connect(turret_config['report_url'])
+
+    def wait_start(self):
+
+        print("Turret online, waiting for HQ orders")
+        while True:
+            recv_str = self.receiver.recv()
+            message = recv_str.split()[1]
+
+            if message == "start":
+                print("Start fire with {nb} cannons".format(nb=self.cannons))
+                self.fire()
+
+    def fire(self):
+
+        threads = []
+        for i in range(self.cannons):
+            space = float(self.rampup) / float(self.cannons)
+
+            if i > 0:
+                time.sleep(space)
+
+            cannon = Cannon(self.start_time, self.run_time, "Turret", self.script_module,
+                            self.script_file, self.sender)
+            cannon.daemon = True
+            threads.append(cannon)
+            cannon.start()
+
+        for thread in threads:
+            timeout = self.run_time - (self.start_time - time.time())
+            if timeout < 0:
+                timeout = 1
+            threads.join(timeout=timeout)
+
+        self.sender.send_json({'stop': True})
+
+
+class Cannon(threading.Thread):
+    """
+    An object representing a user for executing the script
 
     :param process_num: the number of the process
     :param thread_num: the id of the thread
@@ -84,19 +160,15 @@ class Agent(threading.Thread):
     :param script_module: the module containing the script to execute
     :param console_logging: boolean to know if we must log inside the console
     """
-    def __init__(self, process_num, thread_num, start_time, run_time,
-                 user_group_name, script_module, script_file, console_logging):
+    def __init__(self, start_time, run_time,
+                 user_group_name, script_module, script_file, sender):
         threading.Thread.__init__(self)
-        self.process_num = process_num
-        self.thread_num = thread_num
         self.start_time = start_time
         self.run_time = run_time
         self.user_group_name = user_group_name
         self.script_module = script_module
         self.script_file = script_file
-        self.console_logging = console_logging
-        self.error_count = 0
-        self.trans_count = 0
+        self.sender = sender
 
         # choose most accurate timer to use (time.clock has finer granularity
         # than time.time on windows, but shouldn't be used on other systems).
@@ -115,6 +187,8 @@ class Agent(threading.Thread):
         trans.process_num = self.process_num
 
         while elapsed < self.run_time:
+            error_count = 0
+            trans_count = 0
             error = ''
             start = self.default_timer()
 
@@ -132,78 +206,17 @@ class Agent(threading.Thread):
             # don't include cleaning time into reports
             trans.br.clean_session()
 
-            # update shared buffer
-            buff[0] += 1
-
             epoch = time.mktime(time.localtime())
 
             if error != '':
                 # Convert line breaks to literal \n so the CSV will be readable.
                 error = '\\n'.join(error.splitlines())
-                self.error_count += 1
-                buff[1] += 1
-            logger.info('%.3f|%i|%s|%f|%s|%s' % (elapsed, epoch,
-                                                 self.user_group_name,
-                                                 scriptrun_time, error,
-                                                 repr(trans.custom_timers).replace(',', '--')))
-            if self.console_logging:
-                print(('%.3f| %i| %s| %.3f| %s| %s' % (elapsed, epoch,
-                                                       self.user_group_name, scriptrun_time,
-                                                       error, repr(trans.custom_timers).replace(',', '--'))))
+                error_count = 1
+            else:
+                trans = 1
+            report_line = '%.3f|%i|%s|%f|%s|%s' % (elapsed, epoch,
+                                                   self.user_group_name,
+                                                   scriptrun_time, error,
+                                                   repr(trans.custom_timers).replace(',', '--'))
 
-
-class UserGroup(object):
-    """
-    This class represent an user group
-    Does not actually run anything
-
-    :param process_num: the id of the process
-    :param thread_num: the id of the thread
-    :param script_file: the path of the tested script
-    :param run_time: the time to run
-    :param rampup: rampup property in seconds
-    :param group_name: the name of the virtual user group
-    :param queue: the queue for child threads
-    """
-    def __init__(self, **kwargs):
-        self.start_time = time.time()
-        self.process_num = kwargs.pop('process_num', None)
-        self.thread_num = kwargs.pop('thread_num', None)
-        self.script_file = kwargs.pop('script_file', None)
-        self.run_time = kwargs.pop('run_time', None)
-        self.rampup = kwargs.pop('rampup', None)
-        self.group_name = kwargs.pop('group_name', None)
-        self.output_dir = kwargs.pop('output_dir', None)
-        self.console_logging = kwargs.pop('console_logging', None)
-
-
-@app.task
-def run_ug(user_group):
-    """
-    Main celery task for running tests
-
-    :param user_group: User group object containing all options
-    :return: None
-    """
-    buff[0] = 0
-    buff[1] = 0
-    script_module = load_script(user_group.script_file)
-    threads = []
-    fh = logging.FileHandler(user_group.output_dir + 'results.csv')
-    logger.addHandler(fh)
-    current_task.update_state(state='RUNNING', meta={'trans': buff[0], 'errors': buff[1]})
-    for i in range(user_group.thread_num):
-        space = float(user_group.rampup) / float(user_group.thread_num)
-        if i > 0:
-            time.sleep(space)
-        agent_thread = Agent(user_group.process_num, i,
-                             user_group.start_time, user_group.run_time,
-                             user_group.group_name,
-                             script_module, user_group.script_file,
-                             user_group.console_logging)
-        agent_thread.daemon = True
-        threads.append(agent_thread)
-        agent_thread.start()
-    while len([t for t in threads if t.is_alive()]) > 0:
-        current_task.update_state(state='RUNNING', meta={'trans': buff[0], 'errors': buff[1]})
-        time.sleep(1)
+            self.sender.send_json({'trans': trans_count, 'errors': error_count, 'report': report_line})
