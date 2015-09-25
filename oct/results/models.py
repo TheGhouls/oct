b6b121dea3dab5d5491e262a843c15a319941eec
karec <manu.valette@gmail.com>
2015-09-25 11:36:22 +0200

    add to_dict method

--- oct/results/models.py
+++ oct/results/models.py
@@ -1,3 +1,4 @@
+import json
 from peewee import Proxy, TextField, FloatField, CharField, SqliteDatabase, Model
 
 db = Proxy()
@@ -11,11 +12,27 @@ class Result(Model):
     custom_timers = TextField(null=True)
     turret_name = CharField(default='Noname')
 
+    def to_dict(self):
+        return {
+            'error': self.error,
+            'scriptrun_time': self.scriptrun_time,
+            'elapsed': self.elapsed,
+            'epoch': self.epoch,
+            'custom_timers': json.loads(self.custom_timers),
+            'turret_name': self.turret_name
+        }
+
     class Meta:
         database = db
 
 
 def set_database(db_path, proxy, config):
+    """Initialize the peewee database with the given configuration
+
+    :param db_path str: the path of the sqlite database
+    :param proxy peewee.Proxy: the peewee proxy to initialise
+    :param config dict: the configuration dictionnary
+    """
     if 'testing' in config and config['testing'] is True:
         database = SqliteDatabase('/tmp/results.sqlite', check_same_thread=False)
     else:
