fe58f1c31242b1dc3bac2f737f674e79aae5ed05
aurelben <aurel.pigot@gmail.com>
2015-10-24 15:53:40 -0700

    funtional oct-pack-turrests command

--- oct/utilities/pack.py
+++ oct/utilities/pack.py
@@ -2,20 +2,40 @@ import argparse
 import json
 import tarfile
 import os.path
+from oct_turrets.utils import is_valid_conf
 
 
 def main():
     parser = argparse.ArgumentParser(description='Give parameters for package a turret config_file and v_user')
     parser.add_argument('oct_project_path', type=str, default='', help='path for oct project dir')
     args = parser.parse_args()
+    path = args.oct_project_path
 
-    
-    pass
+    if os.path.exists(path):
+        config_file_path = path + "config.json"
+        tar_path = tar_me(config_file_path, path)
+    else:
+        print("you need to enter a valid path")
 
 
-def tar_me(path_list):
-    pass
+def tar_me(config_file, dir_path):
+    conf_file = 0
+    if os.path.isfile(config_file):
+        with open(config_file) as f:
+            conf_file = f.read()
 
+    json_parsed = json.loads(conf_file)
+    turrets = json_parsed['turrets']
 
-def other():
-    pass
+    for turret in turrets:
+        # if is_valid_conf(turret):
+        if True:
+            tar_file_name = turret['name']
+            tar_file = tarfile.open(tar_file_name+".tar", 'w')
+            tar_file.add(os.path.abspath(config_file))
+            tar_file.add(os.path.abspath(turret['script']))
+            for f in tar_file.getnames():
+                print("Added %s" % f)
+            print("tar file is: %s" % dir_path+tar_file_name+".tar")
+            tar_file.close()
+
