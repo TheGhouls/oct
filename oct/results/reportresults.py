a3eb760bd37f5c93f3ae9eeefeb5b5f40337f249
evalette <emmanuel.valette@milibris.com>
2015-11-23 11:25:56 +0100

    remove unused dict_factory

--- oct/results/reportresults.py
+++ oct/results/reportresults.py
@@ -16,13 +16,6 @@ def split_series(points, interval):
     return series
 
 
-def dict_factory(cursor, row):
-    d = {}
-    for idx, col in enumerate(cursor.description):
-        d[col[0]] = row[idx]
-    return d
-
-
 class Results(object):
     def __init__(self, results_file_name, run_time):
         self.results_file_name = results_file_name
