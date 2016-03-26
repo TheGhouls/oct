429371cd6c05c1a31947df65135028828fbd2e6d
karec <manu.valette@gmail.com>
2016-03-26 19:34:07 +0100

    refactor output function

--- oct/results/output.py
+++ oct/results/output.py
@@ -9,6 +9,47 @@ from oct.results.report import ReportResults
 from oct.results.writer import ReportWriter
 
 
+def generate_graphs(data, name, results_dir):
+    """Generate all reports from original dataframe
+
+    :param dic data: dict containing raw and compiled results dataframes
+    :param str name: name for prefixing graphs output
+    :param str results_dir: results output directory
+    """
+    graphs.resp_graph_raw(data['raw'], name + '_response_times.svg', results_dir)
+    graphs.resp_graph(data['compiled'], name + '_response_times_intervals.svg', results_dir)
+    graphs.tp_graph(data['compiled'], name + '_throughput.svg', results_dir)
+
+
+def print_infos(results):
+    """Print informations in standard output
+
+    :param ReportResults results: the report result containing all compiled informations
+    """
+    print('transactions: %i' % results.total_transactions)
+    print('timers: %i' % results.total_timers)
+    print('errors: %i' % results.total_errors)
+    print('test start: %s' % results.start_datetime)
+    print('test finish: %s\n' % results.finish_datetime)
+
+
+def write_template(data, results_dir, parent):
+    """Write the html template
+
+    :param dict data: the dict containing all data for output
+    :param str results_dir: the ouput directory for results
+    :param str parent: the parent directory
+    """
+    print("Generating html report...")
+    partial = time.time()
+    j_env = Environment(loader=FileSystemLoader(os.path.join(results_dir, parent, 'templates')))
+    template = j_env.get_template('report.html')
+
+    report_writer = ReportWriter(results_dir, parent)
+    report_writer.write_report(template.render(data))
+    print("HTML report generated in {} seconds\n".format(time.time() - partial))
+
+
 def output(results_dir, config, parent='../../'):
     """Write the results output for the given test
 
@@ -27,11 +68,7 @@ def output(results_dir, config, parent='../../'):
         print("No results, cannot create report")
         return False
 
-    print('transactions: %i' % results.total_transactions)
-    print('timers: %i' % results.total_timers)
-    print('errors: %i' % results.total_errors)
-    print('test start: %s' % results.start_datetime)
-    print('test finish: %s\n' % results.finish_datetime)
+    print_infos(results)
 
     data = {
         'report': results,
@@ -43,23 +80,12 @@ def output(results_dir, config, parent='../../'):
 
     print("Generating graphs...")
     partial = time.time()
-    graphs.resp_graph_raw(results.main_results['raw'], 'All_Transactions_response_times.svg', results_dir)
-    graphs.resp_graph(results.main_results['compiled'], 'All_Transactions_response_times_intervals.svg', results_dir)
-    graphs.tp_graph(results.main_results['compiled'], 'All_Transactions_throughput.svg', results_dir)
+    generate_graphs(results.main_results, 'All_Transactions', results_dir)
 
     for key, value in results.timers_results.items():
-        graphs.resp_graph_raw(value['raw'], key + '_response_times.svg', results_dir)
-        graphs.resp_graph(value['compiled'], key + '_response_times_intervals.svg', results_dir)
-        graphs.tp_graph(value['compiled'], key + '_throughput.svg', results_dir)
+        generate_graphs(value, key, results_dir)
     print("All graphs generated in {} seconds\n".format(time.time() - partial))
 
-    print("Generating html report...")
-    partial = time.time()
-    j_env = Environment(loader=FileSystemLoader(os.path.join(results_dir, parent, 'templates')))
-    template = j_env.get_template('report.html')
-
-    report_writer = ReportWriter(results_dir, parent)
-    report_writer.write_report(template.render(data))
-    print("HTML report generated in {} seconds\n".format(time.time() - partial))
+    write_template(data, results_dir, parent)
     print("Full report generated in {} seconds".format(time.time() - start))
     return True
