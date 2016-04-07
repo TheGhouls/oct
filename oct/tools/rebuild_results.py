11df29899bb8ad3e2394bf055127b6d3b70764da
evalette <emmanuel.valette@milibris.com>
2016-04-07 17:50:36 +0200

    fix python 2 compatibility

--- oct/tools/rebuild_results.py
+++ oct/tools/rebuild_results.py
@@ -1,3 +1,5 @@
+import six
+
 from oct.results.output import output
 from oct.results.models import db, set_database
 from oct.utilities.configuration import configure
@@ -10,7 +12,10 @@ def rebuild(args):
 
 
 def rebuild_results(sp):
-    parser = sp.add_parser('rebuild-results', help="Rebuild the html report from result dir", aliases=['rebuild'])
+    if six.PY2:
+        parser = sp.add_parser('rebuild-results', help="Rebuild the html report from result dir")
+    else:
+        parser = sp.add_parser('rebuild-results', help="Rebuild the html report from result dir", aliases=['rebuild'])
     parser.add_argument('results_dir', help="The directory containing the results")
     parser.add_argument('results_file', help="The result file")
     parser.add_argument('config_file', help="The configuration file")
