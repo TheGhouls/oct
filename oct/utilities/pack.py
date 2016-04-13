77dfb3ed237345b5861ccc4c7bdeae2c5353a31b
karec <manu.valette@gmail.com>
2016-04-13 23:51:52 +0200

    improve packaging system, now create compressed archives and took more configuration and extra files if needed

--- oct/utilities/pack.py
+++ oct/utilities/pack.py
@@ -6,66 +6,89 @@ import os.path
 import tempfile
 from jinja2 import Environment, PackageLoader
 
-from oct.utilities.configuration import configure_for_turret
+from oct.utilities.configuration import configure_for_turret, cleanup_turret_config
 
 
-def pack(args):
-    if os.path.exists(args.path):
-        env = Environment(loader=PackageLoader('oct.utilities', 'templates'))
-        turret_setup = env.get_template('scripts/setup.py')
-
-        config_file = os.path.join(os.path.abspath(args.path), "config.json")
-        configs = configure_for_turret(args.path, config_file)
-
-        for turret in configs:
-            content = json.dumps(turret, indent=2)
-            tmp_file = os.path.join(tempfile.gettempdir(), turret['name'] + ".json")
-            tmp_setup = None
-            if args.python:
-                tmp_setup = os.path.join(tempfile.gettempdir(), turret['name'] + ".py")
-                turrets_requirements = turret.get('turrets_requirements', [])
-                setup_file = turret_setup.render({'turrets_requirements': turrets_requirements,
-                                                 'name': turret.get('name')})
-            try:
-                with open(tmp_file, 'w') as f:
-                    f.write(content)
-                if tmp_setup is not None:
-                    with open(tmp_setup, 'w') as f:
-                        f.write(setup_file)
-                pack_turret(turret, tmp_file, os.path.dirname(config_file), args.path, tmp_setup=tmp_setup)
-                os.remove(tmp_file)
-                if tmp_setup is not None:
-                    os.remove(tmp_setup)
-            except IOError as e:
-                print("Error while packaging turret %s" % turret['name'])
-                print("Error: %s" % e)
-    else:
-        print("you need to enter a valid project path")
-        sys.exit(2)
+def cleanup_temp_files(turret, files):
+    for f in files:
+        try:
+            os.remove(f)
+        except IOError as e:
+            print("Error while packagin turret %s" % turret['name'])
+            print("Error: %s" % e)
 
 
-def pack_turret(turret_config, tmp_config_file, base_config_path, path=None, tmp_setup=None):
+def write_temp_files(turret, files):
+    writed_files = []
+    for f in files:
+        try:
+            with open(f['filename'], 'w') as fd:
+                fd.write(f['content'])
+                writed_files.append(f['filename'])
+        except IOError as e:
+            print("Error while packaging turret %s" % turret['name'])
+            print("Error: %s" % e)
+    return writed_files
+
+
+def get_files_and_content(turret, is_python=False):
+    ret = []
+    env = Environment(loader=PackageLoader('oct.utilities', 'templates'))
+    turret_setup = env.get_template('scripts/setup.py')
+
+    if is_python:
+        tmp_setup = os.path.join(tempfile.gettempdir(), turret['name'] + ".py")
+        turrets_requirements = turret.get('turrets_requirements', [])
+        setup_file = turret_setup.render({'turrets_requirements': turrets_requirements,
+                                         'name': turret.get('name')})
+        ret.append({'filename': tmp_setup, 'content': setup_file})
+
+    content = json.dumps(cleanup_turret_config(turret), indent=2)
+    tmp_config = os.path.join(tempfile.gettempdir(), "config.json")
+    ret.append({'filename': tmp_config, 'content': content})
+    return ret
+
+
+def pack_turret(turret, temp_files, base_config_path, path=None,):
     """pack a turret into a tar file based on the turret configuration
 
     :param dict turret_config: the turret configuration to pack
     :param str tmp_config_file: the path of the temp config file
     :param str base_config_path: the base directory of the main configuration file
     """
-    file_name = turret_config['name']
+    file_name = turret['name']
+    files = temp_files + turret.get('extra_files', [])
     if path is not None:
         file_name = os.path.join(path, file_name)
-    tar_file = tarfile.open(file_name + ".tar", 'w')
-    tar_file.add(os.path.abspath(tmp_config_file), arcname="config.json")
-    if tmp_setup is not None:
-        tar_file.add(os.path.abspath(tmp_setup), arcname="setup.py")
+    tar_file = tarfile.open(file_name + ".tar.gz", 'w:gz')
 
-    script_path = os.path.join(os.path.abspath(base_config_path), turret_config['script'])
-    tar_file.add(script_path, arcname=turret_config['script'])
+    for f in files:
+        tar_file.add(os.path.abspath(f), arcname=os.path.basename(f))
+
+    script_path = os.path.join(os.path.abspath(base_config_path), turret['script'])
+    tar_file.add(script_path, arcname=turret['script'])
+
     for f in tar_file.getnames():
         print("Added %s" % f)
     tar_file.close()
-    print("Archive %s created" % (file_name + ".tar"))
+    print("Archive %s created" % (tar_file.name))
     print("=========================================")
+
+
+def pack(args):
+    if os.path.exists(args.path):
+        config_file = os.path.join(os.path.abspath(args.path), "config.json")
+        configs = configure_for_turret(args.path, config_file)
+
+        for turret in configs:
+            files = get_files_and_content(turret, args.python)
+            temp_files = write_temp_files(turret, files)
+            pack_turret(turret, temp_files, os.path.dirname(config_file), args.path)
+            cleanup_temp_files(turret, temp_files)
+
+    else:
+        print("you need to enter a valid project path")
+        sys.exit(2)
 
 
 def pack_turrets(sp):
