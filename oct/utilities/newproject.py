2a64345e0adb01cf3c63f48c0f20c70510c25918
karec <manu.valette@gmail.com>
2016-04-25 23:59:24 +0200

    remove nonsense

--- oct/utilities/newproject.py
+++ oct/utilities/newproject.py
@@ -20,7 +20,6 @@ def check_template(members):
 
     for fname in required_members:
         assert fname in members, "Required file %s not in archive" % fname
-        return False
 
     return True
 
