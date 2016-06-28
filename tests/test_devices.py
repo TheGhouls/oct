04310fb40af60a18ba3a9f15e26fae34c96b0fda
karec <manu.valette@gmail.com>
2016-06-28 23:08:30 +0200

    improve devices tests

--- tests/test_devices.py
+++ tests/test_devices.py
@@ -2,6 +2,14 @@ import unittest
 from multiprocessing import Process
 
 from oct.core.devices import forwarder, streamer
+from oct.utilities.run_device import start_device, run_device
+
+
+class DummyArgs:
+
+    device = 'forwarder'
+    frontend = 0
+    backend = 0
 
 
 class DevicesTest(unittest.TestCase):
@@ -11,7 +19,7 @@ class DevicesTest(unittest.TestCase):
         """
         p = Process(target=forwarder, args=(0, 0))
         p.start()
-        p.join(timeout=2)
+        p.join(timeout=1)
         p.terminate()
 
     def test_streamer(self):
@@ -19,5 +27,21 @@ class DevicesTest(unittest.TestCase):
         """
         p = Process(target=streamer, args=(0, 0))
         p.start()
-        p.join(timeout=2)
+        p.join(timeout=1)
+        p.terminate()
+
+    def test_start_device_function(self):
+        """Should be able to start device with start_device function
+        """
+        p = Process(target=start_device, args=('streamer', 0, 0))
+        p.start()
+        p.join(timeout=1)
+        p.terminate()
+
+    def test_run_device_function(self):
+        """Should be able start device with run_device function
+        """
+        p = Process(target=start_device, args=('forwarder', 0, 0))
+        p.start()
+        p.join(timeout=1)
         p.terminate()
