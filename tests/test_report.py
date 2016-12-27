7146dd96a2d39cb0f344ea6e63faab6415e54507
emmanuel valette <emmanuel.valette@storetail.io>
2016-12-27 11:00:38 +0100

    Working tests with new sqlite backend store
    see #35

--- tests/test_report.py
+++ tests/test_report.py
@@ -2,7 +2,7 @@ import os
 import unittest
 
 from oct.results.report import ReportResults
-from oct.results.models import set_database, db
+from oct.result_backends.sqlite import SQLiteLoader
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
@@ -12,6 +12,12 @@ class ReportTest(unittest.TestCase):
     def test_empty_results(self):
         """Test report with empty results
         """
-        set_database(os.path.join(BASE_DIR, 'fixtures', 'empty_results.sqlite'), db, {})
-        report = ReportResults(60, 10)
+        config = {
+            'testing': False,
+            'results_database': {
+                'db_uri': os.path.join(BASE_DIR, 'fixtures', 'empty_results.sqlite')
+            }
+        }
+        loader = SQLiteLoader(config, '')
+        report = ReportResults(60, 10, loader)
         report.compile_results()
