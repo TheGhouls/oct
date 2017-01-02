8a808ebfb02144ba80b45c9b8fc23668c44e9fa8
emmanuel valette <emmanuel.valette@storetail.io>
2017-01-02 15:09:17 +0100

    Remove unused and legacy exceptions
    (#42)

--- oct/core/exceptions.py
+++ oct/core/exceptions.py
@@ -10,35 +10,3 @@ class OctGenericException(Exception):
 
     """
     pass
-
-
-class FormNotFoundException(Exception):
-    """
-    Raised in case of FormNotFound with browser
-
-    """
-    pass
-
-
-class NoUrlOpen(Exception):
-    """
-    Raised in case of no url open but requested inside browser class
-
-    """
-    pass
-
-
-class LinkNotFound(Exception):
-    """
-    Raised in case of link not found in current html document
-
-    """
-    pass
-
-
-class NoFormWaiting(Exception):
-    """
-    Raised in case of action required form if no form selected
-
-    """
-    pass
