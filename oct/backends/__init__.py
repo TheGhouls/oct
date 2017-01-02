6c7226c1e8c8df1a168d3560e5008bd001a8203e
emmanuel valette <emmanuel.valette@storetail.io>
2017-01-02 18:02:44 +0100

    Remove redis store and loader from default import in init package
    This will allow to make smaller imports without requiring to install redis.
    Redis store and loader should now be import with `from oct.backends.redis import...`

--- oct/backends/__init__.py
+++ oct/backends/__init__.py
@@ -1,13 +1,10 @@
 from oct.backends.base import BaseStore, BaseLoader
-from oct.backends.redis import RedisStore, RedisLoader
 from oct.backends.dummy import DummyStore, DummyLoader
 from oct.backends.sqlite import SQLiteStore, SQLiteLoader
 
 __all__ = [
     'BaseStore',
     'BaseLoader',
-    'RedisStore',
-    'RedisLoader',
     'DummyStore',
     'DummyLoader',
     'SQLiteStore',
