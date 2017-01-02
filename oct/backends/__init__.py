bdfcbc324b5d8c278a52b59199b24bacb92f04f5
emmanuel valette <emmanuel.valette@storetail.io>
2017-01-02 16:20:47 +0100

    Add base backend to init

--- oct/backends/__init__.py
+++ oct/backends/__init__.py
@@ -1,8 +1,11 @@
+from oct.backends.base import BaseStore, BaseLoader
 from oct.backends.redis import RedisStore, RedisLoader
 from oct.backends.dummy import DummyStore, DummyLoader
 from oct.backends.sqlite import SQLiteStore, SQLiteLoader
 
 __all__ = [
+    'BaseStore',
+    'BaseLoader',
     'RedisStore',
     'RedisLoader',
     'DummyStore',
